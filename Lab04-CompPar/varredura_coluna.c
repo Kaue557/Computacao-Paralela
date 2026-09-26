7c0c508a34320559f6a5b1335c3ba2b7faa68656
Kaue557 <kauelima557@gmail.com>
2026-09-26 01:43:00 -0300

    pasta Lab04

---
+++ Lab04-CompPar/varredura_coluna.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <N>\n", argv[0]);
+        return 1;
+    }
+    
+    int N = atoi(argv[1]);
+    double *A = (double*) malloc(N * N * sizeof(double));
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i * N + j] = (double)(i + j);
+        }
+    }
+
+    long pares = 0;
+    struct timespec inicio, fim;
+
+    clock_gettime(CLOCK_MONOTONIC, &inicio);
+
+    // Ordem de Coluna (Column-Major)
+    for (int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i++) {
+            if ((int)A[i * N + j] % 2 == 0) {
+                pares++;
+            }
+        }
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &fim);
+    
+    double tempo = (fim.tv_sec - inicio.tv_sec) + 
+                   (fim.tv_nsec - inicio.tv_nsec) / 1e9;
+
+    printf("[Varredura Coluna] Pares: %ld | Tempo: %.6f segundos\n", pares, tempo);
+    
+    free(A);
+    
+    return 0;
+}
